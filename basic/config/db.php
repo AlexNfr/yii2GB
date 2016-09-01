39628288fde53c82cba8fd1879ffff2547bb890e
AlexNfr <nfrcaptain@yandex.ru>
2016-09-01 03:03:57 +0300

    Master: Начальный коммит.

---
+++ basic/config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=yii2DB',
+    'username' => 'yii2_user',
+    'password' => '',
+    'charset' => 'utf8',
+];
